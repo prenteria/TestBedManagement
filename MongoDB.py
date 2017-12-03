f0766ded831bdaae57660907428053f87c03e1f9
gigialmo88 <gmalmodovar@miners.utep.edu>
2017-12-02 19:55:43 -0700

    Update MongoDB.py

--- MongoDB.py
+++ MongoDB.py
@@ -37,6 +37,16 @@ def deleteUser(name):
     except Exception, e:
 	    print str(e)
 
+
+def insertWorkshopGroup(name, description, type, skill_level, published_date, session_type):
+def deleteWorkshopGroup(name):
+def updateWorkshopGroup(name):
+
+def insertWorkshopUnit(name, description, type, skill_level, published_date, session_type, connection_string_type):
+def deleteWorkshopUnit(name):
+def updateWorkshopUnit(name):
+
+
 def main():
     
     while(1):
